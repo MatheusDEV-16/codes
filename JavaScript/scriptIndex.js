328ccae160cbeebb7ce6f15bbc7e9fde1e5d2d57
MatheusDEV-16 <114534927+MatheusDEV-16@users.noreply.github.com>
2022-11-06 10:25:04 -0300

    update

--- JavaScript/scriptIndex.js
+++ JavaScript/scriptIndex.js
@@ -1,11 +1,18 @@
+var dialog = window.document.getElementById('msg')
 function enviar() {
     var user = window.document.getElementById('login')
     var pass = window.document.getElementById('pass')
     if (user.value == '' && pass.value == '') {
-        window.alert('Preencha os campos: "Login" e "Senha"')
+        dialog.showModal()
+        dialog.innerHTML = '<h1>Não foi possível acessar sua conta :(</h1><h2>Preencha os campos: <strong>Login</strong> e <strong>Senha</strong><h2><button id="beforeBtn" onclick="before()">Voltar</button>'
+        
     } else if (user.value == '') {
         window.alert('Preencha o campo "Login"')
     } else if (pass.value == '' && pass.value == '') {
         window.alert('Preencha o campo "Senha"')
     }
+}
+
+function before() {
+    dialog.close()
 }
