643c9b96ef56ceaf8b30b3181174bb1d4a373596
MatheusDEV-16 <114534927+MatheusDEV-16@users.noreply.github.com>
2022-11-07 08:59:57 -0300

    update

--- JavaScript/scriptIndex.js
+++ JavaScript/scriptIndex.js
@@ -1,4 +1,7 @@
 var dialog = window.document.getElementById('msg')
+var res = window.document.getElementById('res')
+const eyeSvg = window.document.getElementById('showPassUser')
+const password = window.document.getElementById('pass')
 function enviar() {
     var user = window.document.getElementById('login')
     var pass = window.document.getElementById('pass')
@@ -7,12 +10,34 @@ function enviar() {
         dialog.innerHTML = '<h1>Não foi possível acessar sua conta :(</h1><h2>Preencha os campos: <strong>Login</strong> e <strong>Senha</strong><h2><button id="beforeBtn" onclick="before()">Voltar</button>'
         
     } else if (user.value == '') {
-        window.alert('Preencha o campo "Login"')
-    } else if (pass.value == '' && pass.value == '') {
-        window.alert('Preencha o campo "Senha"')
+        dialog.showModal()
+        dialog.innerHTML = '<h1>Não foi possível acessar sua conta :(</h1><h2>Preencha o campo: <strong>Login</strong></h2><button id="beforeBtn" onclick="before()">Voltar</button>'
+    } else if (pass.value == '') {
+        dialog.showModal()
+        dialog.innerHTML = '<h1>Não foi possível acessar sua conta :(</h1><h2>Preencha o campo: <strong>Senha</strong></h2><button id="beforeBtn" onclick="before()">Voltar</button>'
     }
 }
 
 function before() {
     dialog.close()
+}
+
+function reveal() {
+    
+    let inputTypeIsPassWord = pass.type == "password"
+    if (inputTypeIsPassWord) {
+        showPassword()
+    } else {
+        hidePassword()
+    }
+}
+
+function showPassword() {
+    password.setAttribute("type", "text")
+    eyeSvg.setAttribute("src", "../images/eye.png")
+}
+
+function hidePassword() {
+    password.setAttribute("type", "password")
+    eyeSvg.setAttribute("src", "../images/showeye.png")
 }
